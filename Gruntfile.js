e260c87c59a15bd65b839ef8ee843924a7e61884
Namhoon Kim <yanhkim@gmail.com>
2013-03-21 16:18:21 +0900

    define test, coverage task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,15 +22,38 @@ module.exports = function(grunt) {
       }
     },
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/specs/**/*.js']
+    },
+    jasmine: {
+      test: {
+        src: ['src/**/*.js'],
+        options: {
+          specs: ['test/specs/**/*.js'],
+          vendor: ['test/vendor/**/*.js']
+        }
+      },
+      coverage: {
+        src: ['src/**/*.js'],
+        options: {
+          specs: ['test/specs/**/*.js'],
+          vendor: ['test/vendor/**/*.js'],
+          template: require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+            coverage: 'output/coverage/coverage.json',
+            report: 'output/coverage'
+          }
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'jasmine:test']);
+  grunt.registerTask('cover', ['jshint', 'jasmine:coverage']);
 
   grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
